fc944f0125db0b4615a37eda8487bbb0f7d702e0
Ahmed479-sketch <45ahmedmehmood@gmail.com>
2025-12-18 03:40:29 +0500

    Update frontend API URL to use AKS backend IP

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,6 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:5000/api' 
-  : 'http://localhost:5000/api';
+const API_URL = 'http://135.171.29.113:5000/api';
 
 // Show message
 function showMessage(message, type = 'success') {
